e0c226442eb1f09261fd814c3aa6d36ff38119db
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2022-01-26 18:22:08 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,217 @@
+﻿#include "ofApp.h"	
+
+//--------------------------------------------------------------
+Actor::Actor(vector<glm::vec3>& location_list, vector<vector<int>>& next_index_list, vector<int>& destination_list) {
+
+	this->select_index = ofRandom(location_list.size());
+	while (true) {
+
+		auto itr = find(destination_list.begin(), destination_list.end(), this->select_index);
+		if (itr == destination_list.end()) {
+
+			destination_list.push_back(this->select_index);
+			break;
+		}
+
+		this->select_index = (this->select_index + 1) % location_list.size();
+	}
+
+	this->next_index = this->select_index;
+
+	this->noise_param = ofRandom(1000);
+	this->noise_step = ofRandom(1000);
+}
+
+//--------------------------------------------------------------
+void Actor::update(const int& frame_span, vector<glm::vec3>& location_list, vector<vector<int>>& next_index_list, vector<int>& destination_list) {
+
+	if (ofGetFrameNum() % frame_span == 0) {
+
+		auto tmp_index = this->select_index;
+		this->select_index = this->next_index;
+		int retry = next_index_list[this->select_index].size();
+		this->next_index = next_index_list[this->select_index][(int)ofRandom(next_index_list[this->select_index].size())];
+		while (--retry > 0) {
+
+			auto destination_itr = find(destination_list.begin(), destination_list.end(), this->next_index);
+			if (destination_itr == destination_list.end()) {
+
+				if (tmp_index != this->next_index) {
+
+					destination_list.push_back(this->next_index);
+					break;
+				}
+			}
+
+			this->next_index = next_index_list[this->select_index][(this->next_index + 1) % next_index_list[this->select_index].size()];
+		}
+		if (retry <= 0) {
+
+			destination_list.push_back(this->select_index);
+			this->next_index = this->select_index;
+		}
+	}
+
+	auto param = ofGetFrameNum() % frame_span;
+	auto distance = location_list[this->next_index] - location_list[this->select_index];
+	this->location = location_list[this->select_index] + distance / frame_span * param;
+
+	if (this->next_index != this->select_index) {
+
+		this->noise_step += 0.1;
+	}
+}
+
+//--------------------------------------------------------------
+glm::vec3 Actor::getLocation() {
+
+	return this->location;
+}
+
+//--------------------------------------------------------------
+float Actor::getWordIndexNoiseValue() {
+
+	return ofNoise(this->noise_param, this->noise_step);
+}
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(30);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(0);
+	ofSetLineWidth(3);
+	ofEnableDepthTest();
+
+	auto span = 60;
+	for (int x = -240; x <= 240; x += span) {
+
+		for (int y = -240; y <= 240; y += span) {
+
+			for (int z = -60; z <= 60; z += span) {
+
+				this->location_list.push_back(glm::vec3(x, y, z));
+			}
+		}
+	}
+
+	for (auto& location : this->location_list) {
+
+		vector<int> next_index = vector<int>();
+		int index = -1;
+		for (auto& other : this->location_list) {
+
+			index++;
+			if (location == other) { continue; }
+
+			float distance = glm::distance(location, other);
+			if (distance <= span) {
+
+				next_index.push_back(index);
+			}
+		}
+
+		this->next_index_list.push_back(next_index);
+	}
+
+	for (int i = 0; i < 230; i++) {
+
+		this->actor_list.push_back(make_unique<Actor>(this->location_list, this->next_index_list, this->destination_list));
+	}
+
+	this->font_size = 46;
+
+	ofTrueTypeFontSettings font_settings("fonts/msgothic.ttc", this->font_size);
+	this->font.loadFont("fonts/Kazesawa-Bold.ttf", this->font_size, true, true, true);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	int frame_span = 10;
+	int prev_index_size = 0;
+
+	if (ofGetFrameNum() % frame_span == 0) {
+
+		prev_index_size = this->destination_list.size();
+	}
+
+	for (auto& actor : this->actor_list) {
+
+		actor->update(frame_span, this->location_list, this->next_index_list, this->destination_list);
+	}
+
+	if (prev_index_size != 0) {
+
+		this->destination_list.erase(this->destination_list.begin(), this->destination_list.begin() + prev_index_size);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->cam.begin();
+
+	string word = "ABCDEFGHEJKLMNOPQRSTUVWXYZ";
+	int sample_count = 120;
+
+	for (auto& actor : this->actor_list) {
+
+		ofPushMatrix();
+		ofTranslate(glm::vec3(actor->getLocation().x, actor->getLocation().y, actor->getLocation().z * 0.25));
+
+		int word_index = ofMap(actor->getWordIndexNoiseValue(), 0, 1, 0, word.size());
+
+		ofPath chara_path = this->font.getCharacterAsPoints(word[word_index], true, false);
+		vector<ofPolyline> outline = chara_path.getOutline();
+
+		ofRotateX(180);
+
+		ofFill();
+		ofSetColor(255, ofMap(actor->getLocation().z, -60, 60, 64, 255));
+		ofBeginShape();
+		for (int outline_index = 0; outline_index < outline.size(); outline_index++) {
+
+			if (outline_index != 0) { ofNextContour(true); }
+
+			auto vertices = outline[outline_index].getResampledByCount(sample_count).getVertices();
+			for (auto& vertex : vertices) {
+
+				auto location = vertex - glm::vec3(this->font_size * 0.5, this->font_size * -0.5, 0);
+				ofVertex(location);
+			}
+		}
+		ofEndShape(true);
+
+		ofNoFill();
+		ofSetColor(0, ofMap(actor->getLocation().z, -60, 60, 64, 255));
+		ofBeginShape();
+		for (int outline_index = 0; outline_index < outline.size(); outline_index++) {
+
+			if (outline_index != 0) { ofNextContour(true); }
+
+			auto vertices = outline[outline_index].getResampledByCount(sample_count).getVertices();
+			for (auto& vertex : vertices) {
+
+				auto location = vertex - glm::vec3(this->font_size * 0.5, this->font_size * -0.5, 0);
+				ofVertex(location);
+			}
+		}
+		ofEndShape(true);
+
+		
+
+		ofPopMatrix();
+	}
+
+	this->cam.end();
+}
+
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
